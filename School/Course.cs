edf6c0a170fcc018b6e305c9387181d06d8ad15a
n1cl4s <niclas.calabrano@gmail.com>
2026-09-10 17:01:14 +0200

    Babysteps enroll

--- School/Course.cs
+++ School/Course.cs
@@ -6,11 +6,20 @@ public List<Student> students;
 
 public Course(string name, int seats)
     {
-        
+    courseName = name;
+    MaxSeats = seats;
+    
     }
 public void Enroll (Student student)
     {
-        
+    if (students.Count >= MaxSeats)
+        {
+            System.Console.WriteLine("Kursen är full");
+        }
+        else
+        {
+            students.Add(student);
+        }
     }
 
 public void Remove (Student student)
