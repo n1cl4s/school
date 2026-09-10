c0c6808816a61f35001b0becdc36481ad3e74b74
n1cl4s <niclas.calabrano@gmail.com>
2026-09-10 16:45:54 +0200

    Metodskelett på plats

--- School/Course.cs
+++ School/Course.cs
@@ -4,4 +4,29 @@ public string courseName;
 public int MaxSeats;
 public List<Student> students;
 
+public Course(string name, int seats)
+    {
+        
+    }
+public void Enroll (Student student)
+    {
+        
+    }
+
+public void Remove (Student student)
+
+    {
+        
+    }
+
+public void RollCall()
+    {
+        
+    }
+
+public void override string ToString()
+    {
+        
+    }
+
 }
