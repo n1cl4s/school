880ed52efea87f3e4dd5a6040466e053ebe91d44
n1cl4s <niclas.calabrano@gmail.com>
2026-09-11 15:47:31 +0200

    Update before break

--- School/Student.cs
+++ School/Student.cs
@@ -2,5 +2,33 @@ class Student
 {
 public string studentName;
 public List<Course> currentCourses = new List<Course>();
+
+public Student(string name)
+{
+    studentName = name;
 }
 
+public void Join(Course course)
+{
+  if (currentCourses.Contains(course))
+    {
+        System.Console.WriteLine($"{studentName} går redan denna kurs");
+    }
+    else
+        {
+            
+        
+        {
+            currentCourses.Add(course); 
+        }
+        if (course.students.Count >= course.MaxSeats)
+        {
+           System.Console.WriteLine("Kursen är tyvärr full!"); 
+        }
+        else
+        {
+            currentCourses.Add(course);
+        }
+        }
+}
+}
