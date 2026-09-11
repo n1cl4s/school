eaa59ffbbe69e3cebb28ad2916845972504933aa
n1cl4s <niclas.calabrano@gmail.com>
2026-09-11 17:02:26 +0200

    Testar metoderna

--- School/Program.cs
+++ School/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var matte = new Course("Matematik", 1);
+
+var anders = new Student("Anders");
+var tommy = new Student("Tommy");
+var niclas = new Student("Niclas");
+
+
+matte.Enroll(anders); 
+anders.Join(matte);
+anders.Leave(matte);
+matte.Enroll(anders);
+matte.RollCall();
